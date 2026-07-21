d8fc47a965ad7b195d6e03cce831235b40c3e840
SanketB2004 <bhosalesanket1507@gmail.com>
2026-07-21 12:33:32 +0530

    add package example

--- inknowledge.java/Packages.java
+++ inknowledge.java/Packages.java
@@ -1,3 +1,5 @@
+package inknowledge;
+
 public class Packages {
     
 }
---
+++ src/Packages.java
@@ -0,0 +1,13 @@
+package src;
+import java.util.Scanner;
+
+public class Packages {
+    
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        
+    }
+}
